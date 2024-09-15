cd53138c440a098ec9cdc13934018a97943b609a
sanjit-shelke <sanjitshelke@gmail.com>
2024-09-15 13:37:36 +0530

    Implementing Exception Handling -404 Resource Not Found

--- SocialMedia/src/main/java/com/webservices/SocialMedia/User/UserDaoService.java
+++ SocialMedia/src/main/java/com/webservices/SocialMedia/User/UserDaoService.java
@@ -23,10 +23,18 @@ public class UserDaoService {
 		return users;	
 	}
 	
+	/*
 	public User findOne(int id)
 	{
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();	
+	}
+	*/
+	
+	public User findOne(int id)
+	{
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().orElse(null);	
 	}
 	
 	public User save(User user)
