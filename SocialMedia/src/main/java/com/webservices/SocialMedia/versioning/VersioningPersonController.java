ec52597110296887ba4a99d533871b187d7383a3
sanjit-shelke <sanjitshelke@gmail.com>
2024-09-28 21:16:49 +0530

    Other different type of Versioning

--- SocialMedia/src/main/java/com/webservices/SocialMedia/versioning/VersioningPersonController.java
+++ SocialMedia/src/main/java/com/webservices/SocialMedia/versioning/VersioningPersonController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 	
+	//URL or URI versioning-------------------------
 	@GetMapping(path = "/v1/person")
 	public PersonV1 getFirstVersionOfPerson()
 	{
@@ -17,6 +18,45 @@ public class VersioningPersonController {
 	{
 		return new PersonV2(new Name("Bob","Charlie"));
 	}
+	
+	//Request Parameter versioning----------------------
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter()
+	{
+		return new PersonV1("Bob Charlie");
+	}
 
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter()
+	{
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
+
+	//Header versioning--------------------------------------------
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader()
+	{
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader()
+	{
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
+	
+	//Accept Header versioning--------------------------------------------
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAcceptHeader()
+	{
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonAcceptHeader()
+	{
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
 }
+
 
