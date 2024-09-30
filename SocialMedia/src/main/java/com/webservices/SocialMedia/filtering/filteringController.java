c4a3ffebaa3a1e8ccefcd70371cb7a2287e0b5e5
sanjit-shelke <sanjitshelke@gmail.com>
2024-09-30 22:23:06 +0530

    Dynamic Filtering1

--- SocialMedia/src/main/java/com/webservices/SocialMedia/filtering/filteringController.java
+++ SocialMedia/src/main/java/com/webservices/SocialMedia/filtering/filteringController.java
@@ -3,12 +3,19 @@ package com.webservices.SocialMedia.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class filteringController {
 	
+	/** Static Filtering
 	@GetMapping("/filtering")
 	public SomeBean filtering()
 	{
@@ -20,4 +27,25 @@ public class filteringController {
 	{
 		return Arrays.asList(new SomeBean("value1","value2","value3"),new SomeBean("value4","value5","value6"));
 	}
+	**/
+	
+	//Dynamic Filtering
+	@GetMapping("/filtering")
+	public MappingJacksonValue filtering()
+	{
+		SomeBean someBean = new SomeBean("value1","value2","value3");
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mappingJacksonValue.setFilters(filters);
+		
+		return mappingJacksonValue;
+	}
+
+	@GetMapping("/filtering-list")
+	public List<SomeBean> filteringList()
+	{
+		return Arrays.asList(new SomeBean("value1","value2","value3"),new SomeBean("value4","value5","value6"));
+	}
+	
 }
