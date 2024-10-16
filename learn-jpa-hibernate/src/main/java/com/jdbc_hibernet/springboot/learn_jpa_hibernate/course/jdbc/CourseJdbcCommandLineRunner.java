cb4eea26fc0a8a5f0a364e54476bedae46b0b9ec
sanjit-shelke <sanjitshelke@gmail.com>
2024-10-16 21:51:53 +0530

    INSERT AND DELETE changes

--- learn-jpa-hibernate/src/main/java/com/jdbc_hibernet/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/jdbc_hibernet/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.jdbc_hibernet.springboot.learn_jpa_hibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	
@@ -13,7 +15,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert();
+		repository.insert(new Course(1, "Spring", "UdemyCourse"));
+		repository.insert(new Course(2, "JAVA", "UdemyCourse2"));
+		
+		repository.deleteById(1);
 	}
 
 }
--- learn-jpa-hibernate/src/main/java/com/jdbc_hibernet/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcReository.java
+++ learn-jpa-hibernate/src/main/java/com/jdbc_hibernet/springboot/learn_jpa_hibernate/course/jdbc/CourseJdbcReository.java
@@ -5,16 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.jdbc_hibernet.springboot.learn_jpa_hibernate.course.Course;
+
 @Repository
 public class CourseJdbcReository {
 	
 	@Autowired
 	private JdbcTemplate springJdbcTemplate;
 	
-	private static String INSERT_QUERY = "insert into course (id, name, author) values(1,'LearnSpringBoot','UdemyCourse');";
+	private static String INSERT_QUERY = "insert into course (id, name, author) values(?,?,?);";
+	private static String DELETE_QUERY = "delete from course where id = ?";
 	
-	public void insert()
+	public void insert(Course course)
 	{
-		springJdbcTemplate.update(INSERT_QUERY);
+		springJdbcTemplate.update(INSERT_QUERY,course.getId(),course.getName(),course.getAuthor());
+	}
+	
+	public void deleteById(long id)
+	{
+		springJdbcTemplate.update(DELETE_QUERY,id);
 	}
 }
