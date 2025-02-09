fe3b20814775ac4b8dac38ab1604cb8de548b4be
sanjit-shelke <sanjitshelke@gmail.com>
2025-02-09 18:31:08 +0530

    Feign REST client for services

--- currency-conversion-services/src/main/java/com/udemy/microservices/currency_conversion_services/CurrencyConversionServicesApplication.java
+++ currency-conversion-services/src/main/java/com/udemy/microservices/currency_conversion_services/CurrencyConversionServicesApplication.java
@@ -2,8 +2,11 @@ package com.udemy.microservices.currency_conversion_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServicesApplication {
 
 	public static void main(String[] args) {
