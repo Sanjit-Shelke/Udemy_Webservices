b1f1240b7e56c394dd6d6ea55b700d0a5ece88c3
sanjit-shelke <sanjitshelke@gmail.com>
2025-02-22 22:37:44 +0530

    Exploring Routes with Spring Cloud Gateway

--- api-gateway/src/main/java/com/udemy/microservices/api_gateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/udemy/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -16,15 +16,23 @@ public class ApiGatewayConfiguration {
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder)
 	{
-		Function<PredicateSpec, Buildable<Route>> routeFunction 
-			= p -> p.path("/get")
-				.filters(f-> f
-						.addRequestHeader("MyHeader", "MyUri")
-						.addRequestParameter("Param", "MyValue"))
-				.uri("http://httpbin.org:80"); 
-			
 		return builder.routes()
-				.route(routeFunction)
+				.route(p -> p.path("/get")
+					.filters(f-> f
+							.addRequestHeader("MyHeader", "MyUri")
+							.addRequestParameter("Param", "MyValue"))
+					.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**")
+						.uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-feign/**")
+						.uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-new/**")
+						.filters(f -> f.rewritePath(
+								"/currency-conversion-new/(?<segment>.*)", 
+								"/currency-conversion-feign/${segment}"))
+						.uri("lb://currency-conversion"))
 				.build();
 	}
 }
